529117dea66e864bbeab5544fefffbdf331f5021
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-05-19 17:17:24 -0400

    Refactor the test

--- tests/lib.rs
+++ tests/lib.rs
@@ -3,7 +3,7 @@ extern crate threed_ice_sys as raw;
 
 use std::{env, fs, mem};
 use std::ffi::CString;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use std::sync::mpsc::{self, Receiver};
 use std::thread::{self, JoinHandle};
 use temporary::Directory;
@@ -24,77 +24,69 @@ macro_rules! path_to_c_str(
 
 // https://github.com/copies/3d-ice/blob/master/bin/3D-ICE-Emulator.c
 #[test]
-fn emulator() {
-    let (stack, working_dir) = setup();
-    let current_dir = ok!(env::current_dir());
-    ok!(env::set_current_dir(working_dir.path()));
-
+fn emulator() { setup(|stack| unsafe {
     let (tx, rx) = mpsc::channel();
     let handle = ping(rx);
 
-    unsafe {
-        let mut stkd: StackDescription_t = mem::uninitialized();
-        let mut analysis: Analysis_t = mem::uninitialized();
-        let mut output: Output_t = mem::uninitialized();
-        let mut tdata: ThermalData_t = mem::uninitialized();
+    let mut stkd: StackDescription_t = mem::uninitialized();
+    let mut analysis: Analysis_t = mem::uninitialized();
+    let mut output: Output_t = mem::uninitialized();
+    let mut tdata: ThermalData_t = mem::uninitialized();
 
-        stack_description_init(&mut stkd);
-        analysis_init(&mut analysis);
-        output_init(&mut output);
+    stack_description_init(&mut stkd);
+    analysis_init(&mut analysis);
+    output_init(&mut output);
 
-        let mut error = parse_stack_description_file(
-            path_to_c_str!(stack).as_ptr() as *mut _,
-            &mut stkd, &mut analysis, &mut output,
-        );
-        assert!(error == TDICE_SUCCESS);
+    let mut error = parse_stack_description_file(
+        path_to_c_str!(stack).as_ptr() as *mut _,
+        &mut stkd, &mut analysis, &mut output,
+    );
+    assert!(error == TDICE_SUCCESS);
 
-        assert!(analysis.AnalysisType == TDICE_ANALYSIS_TYPE_TRANSIENT);
+    assert!(analysis.AnalysisType == TDICE_ANALYSIS_TYPE_TRANSIENT);
 
-        error = generate_output_headers(&mut output, stkd.Dimensions,
-                                        str_to_c_str!("% ").as_ptr() as String_t);
-        assert!(error == TDICE_SUCCESS);
+    error = generate_output_headers(&mut output, stkd.Dimensions,
+                                    str_to_c_str!("% ").as_ptr() as String_t);
+    assert!(error == TDICE_SUCCESS);
 
-        thermal_data_init(&mut tdata);
+    thermal_data_init(&mut tdata);
 
-        error = thermal_data_build(&mut tdata, &mut stkd.StackElements, stkd.Dimensions,
-                                   &mut analysis);
-        assert!(error == TDICE_SUCCESS);
+    error = thermal_data_build(&mut tdata, &mut stkd.StackElements, stkd.Dimensions,
+                               &mut analysis);
+    assert!(error == TDICE_SUCCESS);
 
-        loop {
-            match emulate_step(&mut tdata, stkd.Dimensions, &mut analysis) {
-                SimResult_t::TDICE_STEP_DONE => {
-                    generate_output(&mut output, stkd.Dimensions, tdata.Temperatures,
-                                    tdata.PowerGrid.Sources, get_simulated_time(&mut analysis),
-                                    TDICE_OUTPUT_INSTANT_STEP);
-                },
-                SimResult_t::TDICE_SLOT_DONE => {
-                    generate_output(&mut output, stkd.Dimensions, tdata.Temperatures,
-                                    tdata.PowerGrid.Sources, get_simulated_time(&mut analysis),
-                                    TDICE_OUTPUT_INSTANT_STEP);
-                    generate_output(&mut output, stkd.Dimensions, tdata.Temperatures,
-                                    tdata.PowerGrid.Sources, get_simulated_time(&mut analysis),
-                                    TDICE_OUTPUT_INSTANT_SLOT);
-                },
-                SimResult_t::TDICE_END_OF_SIMULATION => break,
-                _ => assert!(false),
-            }
+    loop {
+        match emulate_step(&mut tdata, stkd.Dimensions, &mut analysis) {
+            SimResult_t::TDICE_STEP_DONE => {
+                generate_output(&mut output, stkd.Dimensions, tdata.Temperatures,
+                                tdata.PowerGrid.Sources, get_simulated_time(&mut analysis),
+                                TDICE_OUTPUT_INSTANT_STEP);
+            },
+            SimResult_t::TDICE_SLOT_DONE => {
+                generate_output(&mut output, stkd.Dimensions, tdata.Temperatures,
+                                tdata.PowerGrid.Sources, get_simulated_time(&mut analysis),
+                                TDICE_OUTPUT_INSTANT_STEP);
+                generate_output(&mut output, stkd.Dimensions, tdata.Temperatures,
+                                tdata.PowerGrid.Sources, get_simulated_time(&mut analysis),
+                                TDICE_OUTPUT_INSTANT_SLOT);
+            },
+            SimResult_t::TDICE_END_OF_SIMULATION => break,
+            _ => assert!(false),
         }
-
-        generate_output(&mut output, stkd.Dimensions, tdata.Temperatures, tdata.PowerGrid.Sources,
-                        get_simulated_time(&mut analysis), TDICE_OUTPUT_INSTANT_FINAL);
-
-        thermal_data_destroy(&mut tdata);
-        stack_description_destroy(&mut stkd);
-        output_destroy(&mut output);
     }
+
+    generate_output(&mut output, stkd.Dimensions, tdata.Temperatures, tdata.PowerGrid.Sources,
+                    get_simulated_time(&mut analysis), TDICE_OUTPUT_INSTANT_FINAL);
+
+    thermal_data_destroy(&mut tdata);
+    stack_description_destroy(&mut stkd);
+    output_destroy(&mut output);
 
     ok!(tx.send(true));
     ok!(handle.join());
+})}
 
-    ok!(env::set_current_dir(&current_dir));
-}
-
-fn setup() -> (PathBuf, Directory) {
+fn setup<F>(code: F) where F: Fn(&Path) {
     const CORE: &'static str = "core.flp";
     const MEMORY: &'static str = "mem.flp";
     const STACK: &'static str = "example_transient.stk";
@@ -105,7 +97,11 @@ fn setup() -> (PathBuf, Directory) {
     ok!(fs::copy(find(MEMORY), directory.path().join(MEMORY)));
     ok!(fs::copy(find(STACK), directory.path().join(STACK)));
 
-    (directory.path().join(STACK), directory)
+    let current_path = ok!(env::current_dir());
+
+    ok!(env::set_current_dir(directory.path()));
+    code(&directory.path().join(STACK));
+    ok!(env::set_current_dir(&current_path));
 }
 
 fn find(name: &str) -> PathBuf {
