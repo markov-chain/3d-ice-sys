0063a7f4dfc4a6f432f0954b9e41923e7aaea92a
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-05-20 17:50:25 -0400

    Do not change the current directory when testing

--- tests/support/mod.rs
+++ tests/support/mod.rs
@@ -2,7 +2,7 @@
 
 extern crate temporary;
 
-use std::{env, fs, mem};
+use std::{fs, mem};
 use std::path::{Path, PathBuf};
 use std::sync::mpsc;
 use std::thread;
@@ -56,17 +56,13 @@ pub fn setup_environment<F>(mut code: F) where F: FnMut(&Path) {
     const MEMORY: &'static str = "mem.flp";
     const STACK: &'static str = "example_transient.stk";
 
-    let directory = ok!(Directory::new("3d-ice-sys"));
+    let directory = ok!(Directory::new("threed_ice_sys"));
 
     ok!(fs::copy(find(CORE), directory.path().join(CORE)));
     ok!(fs::copy(find(MEMORY), directory.path().join(MEMORY)));
-    ok!(fs::copy(find(STACK), directory.path().join(STACK)));
+    copy_fixing_paths(find(STACK), directory.path().join(STACK));
 
-    let current_path = ok!(env::current_dir());
-
-    ok!(env::set_current_dir(directory.path()));
     code(&directory.path().join(STACK));
-    ok!(env::set_current_dir(&current_path));
 }
 
 pub fn setup_ping<F>(mut code: F) where F: FnMut() {
@@ -92,4 +88,36 @@ fn find(name: &str) -> PathBuf {
     let path = PathBuf::from("build/source/bin").join(name);
     assert!(fs::metadata(&path).is_ok());
     path
+}
+
+fn copy_fixing_paths<S: AsRef<Path>, D: AsRef<Path>>(source: S, destination: D) {
+    use std::fs::File;
+    use std::io::{BufRead, BufReader, BufWriter, Write};
+
+    let directory = ok!(destination.as_ref().parent());
+
+    let mut source = ok!(File::open(source.as_ref()));
+    let reader = BufReader::new(&mut source);
+
+    let mut destination = ok!(File::create(destination.as_ref()));
+    let mut writer = BufWriter::new(&mut destination);
+
+    for line in reader.lines() {
+        let line = ok!(line);
+        match line.find('"') {
+            Some(i) => match line.rfind('"') {
+                Some(j) => {
+                    ok!(writer.write(&line[..i+1].as_bytes()));
+                    ok!(writer.write(ok!(directory.join(&line[i+1..j]).to_str()).as_bytes()));
+                    ok!(writer.write(&line[j..].as_bytes()));
+                    ok!(writer.write(b"\n"));
+                    continue;
+                },
+                _ => assert!(false),
+            },
+            _ => {},
+        }
+        ok!(writer.write(line.as_bytes()));
+        ok!(writer.write(b"\n"));
+    }
 }
