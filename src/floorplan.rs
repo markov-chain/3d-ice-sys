ec803010e4276ef8f4c75810b54f9ca5e5bf34b6
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-07-11 09:28:15 -0400

    Add fill_sources_floorplan

--- src/floorplan.rs
+++ src/floorplan.rs
@@ -11,3 +11,7 @@ pub struct Floorplan_t {
     pub SurfaceCoefficients: FloorplanMatrix_t,
     pub Bpowers: *mut Power_t,
 }
+
+extern "C" {
+    pub fn fill_sources_floorplan(floorplan: *mut Floorplan_t, sources: *mut Source_t) -> Error_t;
+}
