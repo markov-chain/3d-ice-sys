60d25b96d8ca79c647525f9922f586a3e8b197a0
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-07-20 18:57:47 -0400

    Add a test for a malformatted floorplan

--- tests/lib.rs
+++ tests/lib.rs
@@ -8,6 +8,7 @@ extern crate threed_ice_sys as ffi;
 mod support;
 
 mod test_emulator;
+mod test_invalid;
 mod test_stack;
 mod test_system_matrix;
 mod test_system_vector;
---
+++ tests/test_invalid.rs
@@ -0,0 +1,26 @@
+use ffi::*;
+use std::mem;
+
+#[test]
+fn test_invalid() {
+    let (path, _directory) = ::support::deploy("invalid");
+
+    unsafe {
+        let mut stkd: StackDescription_t = mem::uninitialized();
+        let mut analysis: Analysis_t = mem::uninitialized();
+        let mut output: Output_t = mem::uninitialized();
+
+        stack_description_init(&mut stkd);
+        analysis_init(&mut analysis);
+        output_init(&mut output);
+
+        let result = parse_stack_description_file(path_to_c_str!(path).as_ptr() as *mut _,
+                                                  &mut stkd, &mut analysis, &mut output);
+
+        assert!(result != TDICE_SUCCESS);
+
+        stack_description_destroy(&mut stkd);
+        analysis_destroy(&mut analysis);
+        output_destroy(&mut output);
+    }
+}
