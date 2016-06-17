56cb8255452fa6c2c4bbaf9ad702155979a03765
Travis CI <>
2016-06-17 09:46:07 +0000

    Update the documentation

---
+++ implementors/core/fmt/trait.Debug.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["libc"] = [];implementors["blas_sys"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='enum' href='blas_sys/c/enum.CBLAS_LAYOUT.html' title='blas_sys::c::CBLAS_LAYOUT'>CBLAS_LAYOUT</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='enum' href='blas_sys/c/enum.CBLAS_TRANSPOSE.html' title='blas_sys::c::CBLAS_TRANSPOSE'>CBLAS_TRANSPOSE</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='enum' href='blas_sys/c/enum.CBLAS_UPLO.html' title='blas_sys::c::CBLAS_UPLO'>CBLAS_UPLO</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='enum' href='blas_sys/c/enum.CBLAS_DIAG.html' title='blas_sys::c::CBLAS_DIAG'>CBLAS_DIAG</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='enum' href='blas_sys/c/enum.CBLAS_SIDE.html' title='blas_sys::c::CBLAS_SIDE'>CBLAS_SIDE</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
